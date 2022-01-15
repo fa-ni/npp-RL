8585be628040238ce0c95ef9d55e716367fa6e9e
XX <power.niki@hotmail.de>
2022-01-15 18:00:31 +0100

    updated trainings script

--- src/main/RL/training.py
+++ src/main/RL/training.py
@@ -1,10 +1,12 @@
 import gym
 from gym import register
 
-from stable_baselines3 import PPO, A2C, SAC, TD3, DDPG, DQN
+from stable_baselines3 import PPO, A2C, DDPG
 from stable_baselines3.common.env_util import make_vec_env
 
 # Configuration
+from stable_baselines3.common.vec_env import VecMonitor
+
 env_dict = gym.envs.registration.registry.env_specs.copy()
 env_id = "TestEnv-v1"
 for env in env_dict:
@@ -13,18 +15,27 @@ for env in env_dict:
 
 register(id=env_id, entry_point="envs.env:TestEnv")  # TODO CHECK
 num_cpu = 12
-vec_env = make_vec_env(env_id, n_envs=num_cpu)
 log_dir = "/model"
 
-# Algorithms
-algorithms = [PPO, A2C, DDPG]
 
-# Training
-for item in algorithms:
-    model = item("MlpPolicy", env=vec_env, verbose=1, tensorboard_log="./logs/", device="cpu",).learn(
-        500000,
-        tb_log_name=f"further_multibinary_{item.__name__}",
-        log_interval=100,
-        # n_eval_episodes=1,
-        # eval_freq=1,
-    )
+def train_agent(algorithm, environment, action_wrapper_name: str = None, obs_wrapper_name: str = None):
+    try:
+        environment.reset()
+        model = algorithm("MlpPolicy", env=environment, verbose=1, tensorboard_log="./logs/", device="cpu",).learn(
+            1500000,
+            tb_log_name=f"scenario2_{action_wrapper_name}_{obs_wrapper_name}_{algorithm.__name__}",
+            # n_eval_episodes=1,
+            # eval_freq=1,
+            # callback=event_callback
+        )
+    except Exception as exception:
+        print("Error")
+        print(exception)
+
+
+# Algorithms
+algorithms = [A2C, PPO, DDPG]
+vec_env = make_vec_env(env_id, n_envs=num_cpu)
+vec_env2 = VecMonitor(vec_env)
+for alg in algorithms:
+    train_agent(alg, vec_env2)

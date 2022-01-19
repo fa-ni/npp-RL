5f46f903a5b9800cb7072408243d89d95147dbe3
XX <power.niki@hotmail.de>
2022-01-19 10:14:57 +0100

    added and adjusted the 3 scenarios

--- src/main/RL/envs/scenario1.py
+++ src/main/RL/envs/scenario1.py
@@ -1,17 +1,17 @@
 import numpy as np
 from gym import Env
-from gym.spaces import Box, MultiDiscrete
+from gym.spaces import MultiBinary, Box
 
 from src.main.services.BackgroundStepService import BackgroundStepService
 from src.main.services.ReactorCreatorService import ReactorCreatorService
 
 
-class Scenario3(Env):
+class Scenario2(Env):
     # Scenario 1 with Action Space Option 1 and Observation Space Option1
 
     def __init__(self):
         # 1. moderator_percent 2. WP1 RPM
-        self.action_space = MultiDiscrete([3, 3])
+        self.action_space = Box(np.array([-1, -1]).astype(np.float32), np.array([1, 1]).astype(np.float32))
         self.observation_space = Box(np.array([-1]).astype(np.float32), np.array([1]).astype(np.float32))
         self.length = 250
 
@@ -19,45 +19,29 @@ class Scenario3(Env):
         done = False
         reward = 0
         self.length -= 1
+        self.state.full_reactor.condenser_pump.rpm_to_be_set = 1600
+        self.state.full_reactor.steam_valve1.status = True
+        self.state.full_reactor.water_valve1.status = True
 
-        # Standard/Minimal Actions (Option 1 Actions)
-        moderator_percent_setting = -1 if action[0] == 0 else (0 if action[0] == 1 else 1)
-        wp_rpm_setting = -25 if action[1] == 0 else (0 if action[1] == 1 else 25)
+        # (normalized value + 1)* (max_value/2)
+        # Standard/Minimal Actions
+        # Option 1 Actions
+        moderator_percent_setting = (action[0] + 1) * (100 / 2)
+        wp_rpm_setting = (action[1] + 1) * (2000 / 2)
         self.state.full_reactor.reactor.moderator_percent = (
             100 - self.state.full_reactor.reactor.moderator_percent + moderator_percent_setting
         )
         self.state.full_reactor.water_pump1.rpm_to_be_set += wp_rpm_setting
 
-        # Necessary for Action Space Option 1
-        if len(action) == 2:
-            # This is necessary as you cannot override the state from this environment in any of the wrappers
-            if self.length == 249:
-                self.state.full_reactor.condenser_pump.rpm = 1600
-                self.state.full_reactor.steam_valve1.status = True
-                self.state.full_reactor.water_valve1.status = True
-            self.state.full_reactor.condenser_pump.rpm_to_be_set = 1600
-            self.state.full_reactor.steam_valve1.status = True
-            self.state.full_reactor.water_valve1.status = True
         # Necessary for Action Space Option 2
         if len(action) == 3:  # TODO
-            # This is necessary as you cannot override the state from this environment in any of the wrappers
-            if self.length == 249:
-                self.state.full_reactor.condenser_pump.rpm = 1600
-                self.state.full_reactor.steam_valve1.status = True
-            self.state.full_reactor.condenser_pump.rpm_to_be_set = 1600
-            water_valve_setting = (
-                False if action[2] == 0 else (self.state.full_reactor.water_valve1.status if action[2] == 1 else True)
-            )
+            water_valve_setting = False if action[2] < 0 else True
             self.state.full_reactor.water_valve1.status = water_valve_setting
         # Necessary for Action Space Option 3
-        if len(action) == 5:  # TODO
-            water_valve_setting = (
-                False if action[2] == 0 else (self.state.full_reactor.water_valve1.status if action[2] == 0 else True)
-            )
-            steam_valve_setting = (
-                False if action[3] == 0 else (self.state.full_reactor.steam_valve1.status if action[3] == 0 else True)
-            )
-            condenser_rpm_setting = -25 if action[4] == 0 else (0 if action[4] == 0 else +25)
+        if len(action) == 5:
+            water_valve_setting = False if action[2] < 0 else True
+            steam_valve_setting = False if action[3] < 0 else True
+            condenser_rpm_setting = (action[4] + 1) * (2000 / 2)
             self.state.full_reactor.water_valve1.status = water_valve_setting
             self.state.full_reactor.steam_valve1.status = steam_valve_setting
             self.state.full_reactor.condenser_pump.rpm_to_be_set = condenser_rpm_setting
@@ -92,6 +76,11 @@ class Scenario3(Env):
     def reset(self):
         self.state = None
         self.state = BackgroundStepService(ReactorCreatorService.create_standard_full_reactor())
+        # This variable should not be accessed like this in the normal simulation. Here it is okay because otherwise we
+        # would need to run the timestep method multiple times to reach the same result.
+        self.state.full_reactor.condenser_pump.rpm = 1600
+        self.state.full_reactor.water_valve1.status = True
+        self.state.full_reactor.steam_valve1.status = True
         self.moderator_percent = 100
         self.length = 250
         return np.array([float(0)])

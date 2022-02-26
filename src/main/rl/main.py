27104c4f1eed46c75b71b0b1850898e365676400
Niklas <niklas@Niklass-MacBook-Air.fritz.box>
2022-02-26 10:26:23 +0100

    increased to two runs per config

--- src/main/rl/main.py
+++ src/main/rl/main.py
@@ -3,5 +3,5 @@ from src.main.rl.utils.constants import ALL_SCENARIOS
 from src.main.rl.wrapper.reward_wrapper2 import RewardOption2Wrapper
 
 if __name__ == "__main__":
-    for _ in range(1):
+    for _ in range(2):
         train_all_scenarios(ALL_SCENARIOS, RewardOption2Wrapper, "full_test_26_02")

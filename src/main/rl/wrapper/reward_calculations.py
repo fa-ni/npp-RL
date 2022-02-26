0f96e470771401423e2472cd8f0cdfd58671a356
Niklas <niklas@Niklass-MacBook-Air.fritz.box>
2022-02-26 09:38:43 +0100

    added reward wrapper + tests

--- src/main/rl/wrapper/reward_calculations.py
+++ src/main/rl/wrapper/reward_calculations.py
@@ -1,19 +1,19 @@
-def calculate_reward_for_corridor(lower: int, upper: int, perfect: int, number: int) -> float:
+def calculate_reward_for_corridor(lower: int, upper: int, perfect: int, number: float) -> float:
     if number < lower or number > upper:
-        result = 0
-    else:
-        result = 1
+        result = -1
     # else:
-    #    if number == perfect:
-    #        result = 1
-    #    elif number < perfect:
-    #        result = (number - lower) / (perfect - lower)
-    #    else:
-    #        result = (number - perfect) / (upper - perfect)
+    #    result = 1
+    else:
+        if number == perfect:
+            result = 1
+        elif number < perfect:
+            result = (number - lower) / (perfect - lower)
+        else:
+            result = (upper - number) / (upper - perfect)
     return result
 
 
-def calculate_roofed_reward(power_output: int) -> float:
+def calculate_roofed_reward(power_output: float) -> float:
     if power_output >= 700:
         result = 700 / power_output
     else:
--- src/main/rl/wrapper/reward_wrapper2.py
+++ src/main/rl/wrapper/reward_wrapper2.py
@@ -1,5 +1,6 @@
 import gym as gym
 
+from src.main.rl.utils.utils import is_done
 from src.main.rl.wrapper.reward_calculations import calculate_roofed_reward
 
 
@@ -8,4 +9,8 @@ class RewardOption2Wrapper(gym.RewardWrapper):
         super().__init__(env)
 
     def reward(self, rew: float) -> float:
-        return calculate_roofed_reward(self.state.full_reactor.generator.power)
+        result = 0
+        done = is_done(self.state.full_reactor, self.length)
+        if not done:
+            result = calculate_roofed_reward(self.state.full_reactor.generator.power)
+        return result

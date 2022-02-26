a6f65571e6e58146db3d838933ff66d7bd81b7c7
Niklas <niklas@Niklass-MacBook-Air.fritz.box>
2022-02-26 10:27:13 +0100

    further information for logging

--- src/main/rl/main.py
+++ src/main/rl/main.py
@@ -4,4 +4,4 @@ from src.main.rl.wrapper.reward_wrapper2 import RewardOption2Wrapper
 
 if __name__ == "__main__":
     for _ in range(2):
-        train_all_scenarios(ALL_SCENARIOS, RewardOption2Wrapper, "full_test_26_02")
+        train_all_scenarios(ALL_SCENARIOS, RewardOption2Wrapper, "full_test_26_02_wo_automation")

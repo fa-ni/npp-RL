bc6c5f47b88b73989d0288d086ad340949da2704
Niklas <niklas@Niklass-MacBook-Air.fritz.box>
2022-02-26 10:51:55 +0100

    fixed tests

--- src/tests/rl/test_training.py
+++ src/tests/rl/test_training.py
@@ -3,12 +3,13 @@ import mock
 
 from src.main.rl.utils.constants import ALL_SCENARIOS
 from src.main.rl.training import train_all_scenarios
+from src.main.rl.wrapper.reward_wrapper2 import RewardOption2Wrapper
 
 
 @mock.patch("src.main.rl.training.train_agent")
 def test_number_of_models_with_one_envs(mock_train_agent):
     scenarios = ["src.main.rl.envs.scenario2:Scenario2"]
-    train_all_scenarios(scenarios)
+    train_all_scenarios(scenarios, RewardOption2Wrapper)
     # Count is the following:
     # 2 (ActionWrapper) * 4 (ObservationWrappers) * 3 (Alg) = 24
     # 2 (ActionWrapper) without Obs * 3 (Alg) = 6
@@ -22,5 +23,5 @@ def test_number_of_models_with_one_envs(mock_train_agent):
 
 @mock.patch("src.main.rl.training.train_agent")
 def test_number_of_models_with_three_envs(mock_train_agent):
-    train_all_scenarios(ALL_SCENARIOS)
+    train_all_scenarios(ALL_SCENARIOS, RewardOption2Wrapper)
     assert mock_train_agent.call_count == 45 * 3
--- src/tests/rl/wrapper/test_obs_wrapper4.py
+++ src/tests/rl/wrapper/test_obs_wrapper4.py
@@ -29,46 +29,32 @@ def test_step(set_up):
     actual_step_one = wrapper.step(action=[0, 1])
     actual_step_two = wrapper.step(action=[1, 1])
     actual_step_three = wrapper.step(action=[1, 1])
-    assert len(actual_step_one[0]) == 5
-    assert len(actual_step_two[0]) == 5
+    assert len(actual_step_one[0]) == 6
+    assert len(actual_step_two[0]) == 6
     np.testing.assert_almost_equal(
         actual_step_one[0],
-        np.array(
-            [
-                -1,
-                0.0005,
-                -1,
-                -0.00025,
-                -1,
-            ]
-        ),
+        np.array([-1, 0.0005, -1, -0.00025, -1, 1]),
     )
     np.testing.assert_almost_equal(
         actual_step_two[0],
-        np.array(
-            [
-                -1,
-                0.0,
-                -0.99868,
-                -0.00036,
-                -1,
-            ]
-        ),
+        np.array([-1, 0.0, -0.99868, -0.00036, -1, 1]),
         5,
     )
-    np.testing.assert_almost_equal(actual_step_three[0], np.array([-0.995, -0.00089, -0.99611, -0.00064, -0.99980]), 5)
+    np.testing.assert_almost_equal(
+        actual_step_three[0], np.array([-0.995, -0.00089, -0.99611, -0.00064, -0.99980, 1]), 5
+    )
 
 
 @pytest.mark.number(2)
 def test_reset(set_up):
     wrapper = set_up
     actual = wrapper.reset()
-    assert all(actual == np.array([-1, 0, -1, 0, -1]))
+    assert all(actual == np.array([-1, 0, -1, 0, -1, 1]))
 
 
 @pytest.mark.number(3)
 def test_obs_space(set_up):
     wrapper = set_up
     assert wrapper.observation_space == Box(
-        np.array([-1, -1, -1, -1, -1]).astype(np.float32), np.array([1, 1, 1, 1, 1]).astype(np.float32)
+        np.array([-1, -1, -1, -1, -1, -1]).astype(np.float32), np.array([1, 1, 1, 1, 1, 1]).astype(np.float32)
     )

e3a9d147181e5ab2fbbf7299a484a60c77dfb6d1
Niklas <niklas@Niklass-Air.fritz.box>
2022-03-07 18:52:50 +0100

    improved evaluation methods

---
+++ src/main/rl/utils/combined_parser.py
@@ -0,0 +1,12 @@
+from src.main.rl.utils.constants import alg_mapping
+from src.main.rl.utils.parser import parse_scenario_name, parse_alg_name, parse_wrapper
+from src.main.rl.utils.utils import WrapperMaker
+
+
+def parse_information_from_path(path: str):
+    scenario_name = parse_scenario_name(path)
+    result_scenario = f"envs.{scenario_name}:{scenario_name.capitalize()}"
+    alg = alg_mapping[parse_alg_name(path)]
+    action_wrapper, obs_wrapper, reward_wrapper = parse_wrapper(path)
+    wrapper_maker = WrapperMaker(action_wrapper, obs_wrapper, reward_wrapper)
+    return result_scenario, alg, wrapper_maker

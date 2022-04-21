df1815352a4c8bf458f5d447303d7dd2cabc1ce9
XX <power.niki@hotmail.de>
2022-04-21 12:26:42 +0200

    changed evaluation method to return much more information that is needed for further evaluation

--- src/main/rl/evaluation/eval.py
+++ src/main/rl/evaluation/eval.py
@@ -1,87 +1,17 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as plticker
 import numpy as np
 from gym import register
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
 
 from src.main.rl.evaluation.criticality_helper import calculate_criticality_score_with_reward_functions
+from src.main.rl.evaluation.plots.phase3_plots import plot_actions_taken, plot_observations
 from src.main.rl.evaluation.sop import get_actions_sop
 from src.main.rl.utils.combined_parser import parse_information_from_path
-from src.main.rl.utils.constants import (
-    scaling_factors_scenario_1,
-    scaling_factors_scenario2,
-    action_dimensions,
-    obs_scaling_factors,
-    obs_dimensions,
-    scaling_factors_scenario3,
-)
-from src.main.rl.utils.parser import parse_scenario_name
 from src.main.rl.utils.utils import WrapperMaker, delete_env_id
 from src.main.rl.wrapper.action_wrapper3 import ActionSpaceOption3Wrapper
+from src.main.rl.wrapper.obs_wrapper4 import ObservationOption4Wrapper
+from src.main.rl.wrapper.obs_wrapper5 import ObservationOption5Wrapper
 from src.main.rl.wrapper.reward_wrapper2 import RewardOption2Wrapper
-
-
-def plot_actions_taken(actions_taken: list, scenario_name: str) -> None:
-    action_space_size = len(actions_taken[0][0])
-    fig, ax = plt.subplots(action_space_size, 1, constrained_layout=True)
-    fig.set_figheight(10)
-    actions_positions = [[] for i in range(action_space_size)]
-    # Transformation of actions taken. Final result: Each Action e.g. the first action will be togther in a list and will
-    # be plotted as one graph. Total will look like the following if action_space_ length ==3:
-    # [[all actions from position 0],[all actions from position 1],[all actions from position 2]]
-    for item in actions_taken:
-        [actions_positions[idx].append(single_action) for idx, single_action in enumerate(item[0])]
-
-    # Scaling to real values as the action space is also normalized to [-1,1]
-    scaled_values = []
-    parsed_scenario_name = parse_scenario_name(scenario_name)
-    if parsed_scenario_name == "scenario1":
-        for idx, item in enumerate(actions_positions):
-            # TODO double check
-            scaled_values.append([int(round((x + 1) * (scaling_factors_scenario_1[idx] / 2))) for x in item])
-    if parsed_scenario_name == "scenario2":
-        for idx, item in enumerate(actions_positions):
-            scaled_values.append(
-                [-scaling_factors_scenario2[idx] if x == 0 else scaling_factors_scenario2[idx] for x in item]
-            )
-    if parsed_scenario_name == "scenario3":
-        for idx, item in enumerate(actions_positions):
-            scaled_values.append([scaling_factors_scenario3[idx][x] for x in item])
-
-    # Plotting
-    [ax[idx].plot(action_position) for idx, action_position in enumerate(scaled_values)]
-    [ax[idx].xaxis.set_major_locator(plticker.MaxNLocator(11)) for idx in range(action_space_size)]
-    [ax[idx].set_title(action_dimensions[idx]) for idx in range(action_space_size)]
-    [ax[idx].set_xlabel("Episode") for idx in range(action_space_size)]
-    [ax[idx].set_ylabel("Value") for idx in range(action_space_size)]
-    plt.show()
-
-
-def plot_observations(observations_taken: list) -> None:
-    obs_space_size = len(observations_taken[0][0])
-    fig, ax = plt.subplots(obs_space_size, 1, constrained_layout=True)
-    obs_positions = [[] for i in range(obs_space_size)]
-    fig.set_figheight(20)
-    # Transformation of actions taken. Final result: Each Action e.g. the first action will be togther in a list and will
-    # be plotted as one graph. Total will look like the following if obs_space_ length ==3:
-    # [[all obs from position 0],[all obs from position 1],[all obs from position 2]]
-    for item in observations_taken:
-        [obs_positions[idx].append(single_action) for idx, single_action in enumerate(item[0])]
-    # Scaling
-    scaled_values_obs = []
-    current_obs_scaling_factors = obs_scaling_factors[obs_space_size]
-    current_obs_dimensions = obs_dimensions[obs_space_size]
-    for idx, item in enumerate(obs_positions):
-        scaled_values_obs.append([int(round((x + 1) * (current_obs_scaling_factors[idx] / 2))) for x in item])
-        # Plotting
-    [ax[idx].plot(obs_position) for idx, obs_position in enumerate(scaled_values_obs)]
-    [ax[idx].set_title(current_obs_dimensions[idx]) for idx in range(obs_space_size)]
-    [ax[idx].set_xlabel("Episode") for idx in range(obs_space_size)]
-    [ax[idx].yaxis.set_major_locator(plticker.MaxNLocator(5)) for idx in range(obs_space_size)]
-    [ax[idx].xaxis.set_major_locator(plticker.MaxNLocator(11)) for idx in range(obs_space_size)]
-    [ax[idx].set_ylabel("Value") for idx in range(obs_space_size)]
-    plt.show()
 
 
 def evaluate(
@@ -103,83 +33,78 @@ def evaluate(
         env_kwargs={"starting_state": starting_state, "length": episode_length},
     )
 
-    mean_reward_over_multiple_evaluations = []
+    rewards_per_timestep = []
     model = alg.load(path)
     obs = vec_env.reset()
     actions_taken = []
     observations_taken = []
     reactor_status_over_time = []
-    observations_taken.append(obs)
-    for _ in range(episode_length):
+    for i in range(episode_length):
         # vec_env.render()
+        observations_taken.append(obs[0])
 
         action, _states = model.predict(obs, deterministic=True)
-        actions_taken.append(action)
+        actions_taken.append(action[0])
         obs, reward, done, info = vec_env.step(action)
         reactor_status_over_time.append(info[0])
-        observations_taken.append(obs)
-        mean_reward_over_multiple_evaluations.append(reward)
+        rewards_per_timestep.append(reward)
         if done:
-            plot_actions_taken(actions_taken, scenario_name)
-            # plot_observations(observations_taken)
+            # plot_actions_taken(actions_taken, scenario_name)
+            # plot_observations(observations_taken[:-1])
             criticality_score = calculate_criticality_score_with_reward_functions(reactor_status_over_time)
             print(f"Criticality Score1: {criticality_score}")
             # criticality_of_states = prepare_critical_states_analysis(reactor_status_over_time)
             # print(f"Criticality Score2: {calculate_score(criticality_of_states)}")
             # print(sum(mean_reward_over_multiple_evaluations))
-            result = sum(mean_reward_over_multiple_evaluations)
+            result = sum(rewards_per_timestep)
             print(result)
-            mean_reward_over_multiple_evaluations = []
             obs = vec_env.reset()
-            actions_taken = []
-            observations_taken = []
-            return result[0], criticality_score
+            return result[0], criticality_score, i + 1, actions_taken, observations_taken, info
 
 
 def evaluate_sop():
     env_id = "TestEnv-v1"
     delete_env_id(env_id)
-
-    register(id=env_id, entry_point="envs.scenario1:Scenario1")
-    wrapper_maker = WrapperMaker(ActionSpaceOption3Wrapper, None, None, RewardOption2Wrapper)
+    scenario_name = "scenario1"
+    register(id=env_id, entry_point="src.main.rl.envs.scenario1:Scenario1")
+    wrapper_maker = WrapperMaker(ActionSpaceOption3Wrapper, None, ObservationOption4Wrapper, RewardOption2Wrapper)
     vec_env = make_vec_env(env_id, n_envs=1, wrapper_class=wrapper_maker.make_wrapper)
-    mean_reward_over_multiple_evaluations = []
+    rewards_per_timestep = []
     obs = vec_env.reset()
     actions_taken = []
     observations_taken = []
-    observations_taken.append(obs)
+    reactor_status_over_time = []
+
     for i in range(250):
-        # vec_env.render()
-        # Evaluate
-        # Interesting if deterministic==True, we always get the same reward, otherwise not
-        action = np.array([get_actions_sop(249 - i)])
-        actions_taken.append(action)
+        observations_taken.append(obs[0])
+        action = np.array([get_actions_sop(250 - i)])
         obs, reward, done, info = vec_env.step(action)
-        observations_taken.append(obs)
-        mean_reward_over_multiple_evaluations.append(reward)
+        actions_taken.append(action[0])
+        if i == 5:
+            print("x")
+        reactor_status_over_time.append(info[0])
+        rewards_per_timestep.append(reward)
         if done:
             # plot_actions_taken(actions_taken, scenario_name)
-            plot_observations(observations_taken)
-
-            print(sum(mean_reward_over_multiple_evaluations))
-            result = sum(mean_reward_over_multiple_evaluations)
-            mean_reward_over_multiple_evaluations = []
+            # plot_observations(observations_taken[:-1])
+            criticality_score = calculate_criticality_score_with_reward_functions(reactor_status_over_time)
+            print(f"Criticality Score1: {criticality_score}")
+            print(sum(rewards_per_timestep))
+            result = sum(rewards_per_timestep)
             obs = vec_env.reset()
-            actions_taken = []
-            observations_taken = []
-            return result
+            return result, criticality_score, i + 1, actions_taken, observations_taken, info
 
 
 # evaluate_sop()
-# path = "../scenario1/training_18_03/scenario1_ActionSpaceOption3Wrapper_ObservationOption4Wrapper_NPPAutomationWrapper_RewardOption2Wrapper_TD3_training_18_03_1/best_model.zip"
-# path = "../models/models/scenario3/training_04_06/scenario3_ActionSpaceOption3Wrapper_ObservationOption5Wrapper_NPPAutomationWrapper_RewardOption2Wrapper_PPO_training_04_06_1/best_model.zip"
-#
-# path = "../models/models/scenario2/training_04_06/scenario2_ActionSpaceOption3Wrapper_ObservationOption5Wrapper_None_RewardOption2Wrapper_PPO_training_04_06_1/best_model.zip"
-#
+# path = "../models/scenario1/training_04_06/scenario1_ActionSpaceOption3Wrapper_ObservationOption4Wrapper_None_RewardOption2Wrapper_TD3_training_04_06_5/best_model.zip"
+###path = "../models/scenario3/training_04_06/scenario3_ActionSpaceOption3Wrapper_ObservationOption5Wrapper_NPPAutomationWrapper_RewardOption2Wrapper_PPO_training_04_06_1/best_model.zip"
+####
+#### path = "../models/models/scenario2/training_04_06/scenario2_ActionSpaceOption3Wrapper_ObservationOption5Wrapper_None_RewardOption2Wrapper_PPO_training_04_06_1/best_model.zip"
+####
 # scenario, alg, wrapper_maker = parse_information_from_path(path)
 # evaluate(
-#    scenario,
-#    path,
-#    alg,
-#    wrapper_maker,
+#   scenario,
+#   path,
+#   alg,
+#   wrapper_maker,episode_length=250
 # )  # starting_state=create_starting_state_option3a(), episode_lgenth=250)

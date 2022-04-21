0a3b78d9fab7c825f0683878f431ae2e98dc5ba0
XX <power.niki@hotmail.de>
2022-04-21 23:09:42 +0200

    needed to scale differently than expected, now they wrapper works as expected

--- src/main/rl/evaluation/wrapper/noise/obs_varies_wrapper.py
+++ src/main/rl/evaluation/wrapper/noise/obs_varies_wrapper.py
@@ -5,14 +5,29 @@ from gym import Wrapper
 
 from src.main.rl.utils.utils import fibonacci_of
 
+obs_scaling_factors = {
+    1: [800],
+    3: [800, 2000, 100],
+    7: [800, 2000, 100, 2000, 1, 30],
+    6: [800, 4000, 550, 8000, 180, 30],
+    11: [800, 4000, 550, 8000, 180, 2000, 100, 2000, 1, 1, 30],
+}
+
 
 class ObservationVariesPositiveNoiseWrapper(Wrapper):
     def step(self, action):
         original_result = list(self.env.step(action))
-        # for 5 % deviation in the normal value we need to add/subtract 0.1 from the scaled value
-        new_obs = []
-        for item in original_result[0]:
-            new_obs.append(item + 0.1 if item < 2 else 2)
+        len_obs_space = len(original_result[0])
+        current_obs_scaling_factors = obs_scaling_factors[len_obs_space]
+
+        # Get real values and multiply with 1.05 for 5 %
+        real_values_with_noise = [
+            int(round((item + 1) * (current_obs_scaling_factors[idx] / 2))) * 1.05
+            for idx, item in enumerate(original_result[0])
+        ]
+        # Normalize again
+        new_obs = [2 * (item / current_obs_scaling_factors[idx]) - 1 for idx, item in enumerate(real_values_with_noise)]
+
         original_result[0] = np.array(new_obs)
         return original_result
 
@@ -20,14 +35,20 @@ class ObservationVariesPositiveNoiseWrapper(Wrapper):
 class ObservationVariesNegativeNoiseWrapper(Wrapper):
     def step(self, action):
         original_result = list(self.env.step(action))
-        # for 5 % deviation in the normal value we need to add/subtract 0.1 from the scaled value
-        new_obs = []
-        for item in original_result[0]:
-            new_obs.append(item - 0.1 if item > 0 else 0)
+        len_obs_space = len(original_result[0])
+        current_obs_scaling_factors = obs_scaling_factors[len_obs_space]
+        # Get real values and multiply with 1.05 for 5 %
+        real_values_with_noise = [
+            int(round((item + 1) * (current_obs_scaling_factors[idx] / 2))) * 0.95
+            for idx, item in enumerate(original_result[0])
+        ]
+        # Normalize again
+        new_obs = [2 * (item / current_obs_scaling_factors[idx]) - 1 for idx, item in enumerate(real_values_with_noise)]
         original_result[0] = np.array(new_obs)
         return original_result
 
 
+# TODO
 class ObservationVariesNoiseWrapper1(Wrapper):
     def __init__(self, env):
         super().__init__(env)
@@ -39,24 +60,24 @@ class ObservationVariesNoiseWrapper1(Wrapper):
         original_result = list(self.env.step(action))
         new_obs = []
         if self.length in self.fibonacci:
-            for item in original_result[0]:
-                if self.maths_sign == "negative":
-                    new_obs.append(item - 0.1 if item > 0 else 0)
-                    self.maths_sign = "positive"
-                else:
-                    new_obs.append(item + 0.1 if item < 2 else 2)
+            len_obs_space = len(original_result[0])
+            current_obs_scaling_factors = obs_scaling_factors[len_obs_space]
+            # Get real values and multiply with 1.05 for 5 %
+            if self.maths_sign == "negative":
+                real_values_with_noise = [
+                    int(round((item + 1) * (current_obs_scaling_factors[idx] / 2))) * 0.95
+                    for idx, item in enumerate(original_result[0])
+                ]
+                self.maths_sign = "positive"
+            else:
+                real_values_with_noise = [
+                    int(round((item + 1) * (current_obs_scaling_factors[idx] / 2))) * 1.05
+                    for idx, item in enumerate(original_result[0])
+                ]
+            # Normalize again
+            new_obs = [
+                2 * (item / current_obs_scaling_factors[idx]) - 1 for idx, item in enumerate(real_values_with_noise)
+            ]
+
             original_result[0] = np.array(new_obs)
-        return original_result
-
-
-class ObservationVariesNoiseWrapper2(Wrapper):
-    # Set deviation between -5 % and +5 % randomly per step for all observations
-    def step(self, action):
-        original_result = list(self.env.step(action))
-        new_obs = []
-        # for 5 % deviation in the normal value we need to add/subtract 0.1 from the scaled value
-        factor = random.randint(-10, 10) / 100
-        for item in original_result[0]:
-            new_obs.append(item + factor if item < 2 else 2)
-        original_result[0] = np.array(new_obs)
         return original_result
